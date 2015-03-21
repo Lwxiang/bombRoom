921ad9fc891738aa5f47c22af1937b3b73f7bf3d
Lwxiang <lwxiang1994@gmail.com>
2015-03-22 00:59:23 +0800

    Fix:补全文件

--- game/models.py
+++ game/models.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
+from django.db import models
 
+
+class Game(models.Model):
+    position = models.CharField(max_length=1000)
+    bomb = models.CharField(max_length=2000)
+    wall = models.CharField(max_length=1000)
+    status = models.TextField()
+    no = models.IntegerField(default=0)
+    turn = models.IntegerField(default=0)
+    start = models.BooleanField(default=False)
--- game/views.py
+++ game/views.py
@@ -1,3 +1,77 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+import json
+import random
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.shortcuts import HttpResponseRedirect
+
+from game.models import Game
+from lobby.models import Player, Room
+
+
+def game_init():
+    game = Game()
+
+    # Make the status start
+    game.no = 0
+    game.status = ""
+    game.turn = 0
+
+    # Make the bomb and the wall init
+    game.bomb = '0' * 4 * 4
+    game.wall = '0' * 4 * 4
+    game.save()
+    return game
+
+
+def game_start(request):
+    response = HttpResponse()
+    if not ('uid' in request.session):
+        response.write('Uid is Invalid')
+    else:
+        if not (request.method == 'POST'):
+            response.write('not post')
+        else:
+            host = request.POST.get('host')
+            room = Room.objects.get(host=host)
+            members = room.members.split(';')
+            for i in members:
+                player = Player.objects.get(id=int(i))
+                player.status = "Gaming"
+                player.save()
+            game = room.game
+            game.start = True
+            game.save()
+
+            # Make the default position
+            position = ""
+
+            flag = False
+            for i in range(0, room.num):
+                x = random.randint(0, room.length)
+                y = random.randint(0, room.length)
+                if flag:
+                    position += ';'
+                flag = True
+                position += str(x) + ',' + str(y)
+
+
+def wait_start(request):
+    response = HttpResponse()
+    if not ('uid' in request.session):
+        response.write('Uid is Invalid')
+    else:
+        if not (request.method == 'GET'):
+            response.write('not get')
+
+
+
+
+
+
+
+
+
+
+
