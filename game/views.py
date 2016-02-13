6c3696b2a383e65fa19bad1bdca7652d8cb17589
Lwxiang <lwxiang1994@gmail.com>
2016-02-13 22:59:15 +0800

    fix

--- game/views.py
+++ game/views.py
@@ -171,7 +171,7 @@ def action(request):
                     raise Game.DoesNotExist
                 game.no += 1
                 members = room.members.split(';')
-                return "%d,%s,%s" % (game.turn, members[game.turn], uid)
+                return HttpResponse("%d,%s,%s" % (game.turn, members[game.turn], uid))
                 if members[game.turn] != uid:
                     status = "0"
                 else:
