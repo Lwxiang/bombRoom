2b5f23b47800baf8ebd515dd87ef9e8e87b7cc54
lwxiang <lwxiang1994@gmail.com>
2016-02-25 19:04:11 +0800

    fix color error

--- bombRoom/template/static/js/main.js
+++ bombRoom/template/static/js/main.js
@@ -208,11 +208,7 @@ function gameStart() {
 			outputLog('BOMB ROOM #' + uid + '　房主：' + data.info.name + '　指令上限：' + data.info.energy + '　用户上限：' + data.info.capacity + '　用户列表：' + data.info.players.map(function(elem, index){
 				return '<span style="color: ' + colors[index] + '">' + elem + '</span>';
 			}).join(', ') + ' (' + data.info.num + '/' + data.info.capacity +')', 1);
-			// data.info.ids.filter(function(elem, uid){
-			// 	mycolor = colors[uid];
-			// })[0];
-			mycolor = colors[data.info.uids.indexOf(user.uid)];
-			alert(mycolor);
+                        mycolor = colors[data.info.uids.indexOf(user.uid.toString())];
 			outputLog('<span style="color: ' + mycolor + '">这是您的颜色</span>', 1);
 			start = 1;
 			getLatestMsg();
